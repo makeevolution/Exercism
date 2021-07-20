98651f3e65cd4358df176e6d43e0bb6dddbda690
Sebastian <aldo.sebastian@sogeti.com>
2021-07-20 13:25:54 +0200

    added double linked list

--- csharp/linked-list/LinkedList.cs
+++ csharp/linked-list/LinkedList.cs
@@ -2,27 +2,76 @@ using System;
 
 public class Deque<T>
 {
-    public T Value {get;}
-    public Deque<T> Prev {get;}
-    public Deque<T> Next {get;}
-    public Deque<T> LinkedList<T
+    private Node<T> current;
+    private Node<T> head;
+    
     public void Push(T value)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        Node<T> node = new Node<T>(value);
+        if (this.head == null) // i.e. the first instance of making the list
+        {
+            // initialize everything, including current
+            this.head = node;
+            this.current = this.head;
+        }
+        else
+        {
+            //The following ensures current will always be the last element
+            this.current.Next = node;
+            node.Prev = this.current;
+            this.current = node;
+        }
+        
     }
 
     public T Pop()
     {
-        throw new NotImplementedException("You need to implement this function.");
+        //Remove the element at the end of the list
+        T last = this.current.Data;
+        this.current = this.current.Prev;
+        //this.current.Next = null;
+        return last;
     }
 
     public void Unshift(T value)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        Node<T> node = new Node<T>(value);
+        if (this.head == null) // i.e. the first instance of making the list
+        {
+            // initialize everything, including current
+            this.head = node;
+            this.current = this.head;
+        }
+        else
+        {
+            //The following ensures current will always be the last element
+            //Connect head and the newly created node
+            this.head.Prev = node;
+            node.Next = this.head;
+            //Finally, move head to node
+            this.head = node;
+        }
     }
 
     public T Shift()
     {
-        throw new NotImplementedException("You need to implement this function.");
+        //Remove the element at the start of the list
+        T first = this.head.Data;
+        this.head = this.head.Next;
+        //this.head.Prev = null;
+        return first;
     }
+}
+
+internal class Node<T>
+{
+    public Node<T> Next {get; set;}
+    public Node<T> Prev {get; set;}
+    public T Data {get; set;}
+    
+    public Node(T data)
+    {
+        this.Data = data;
+    }
+    
 }
